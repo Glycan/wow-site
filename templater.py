24fecc355bfaa46d81e93d1fe686183204fa72e9
Matthew Liberman <wisepoison@gmail.com>
2012-12-25 18:13:56 -0500

    did lots of stuff

--- templater.py
+++ templater.py
@@ -1,36 +1,74 @@
 #!/usr/bin/python3
 import json
-import sys
+import yaml
+import markdown
 import os
+import shutil
+from argparse import ArgumentParser
+
+frame = open("templates/frame.html").read()
+header = open("templates/header.html").read()
+bubble = open("templates/bubble.html").read()
+rule = open("templates/rule.css").read()
+
+parser = ArgumentParser(description="""
+Put togeather a site from source files. The contents of the nonhtml folder
+will be copied into the output file. The source files can be in YAML or JSON
+format. Each section will be made into a bubble. Each bubble should have a
+content attribute, a type attribute (which will be it's class attribute),
+and possibly a CSS attribute (which will be it's style attribute). A source
+file can also have a CSS attribute, which will be put into a linked css
+file. A source file should also have a title attribute, which will be it's
+title
+""")
+parser.add_argument("source", default="source", nargs="?",
+    help="a directory with the source files in JSON or YAML, defaults to source")
+parser.add_argument("dest", default="output", nargs="?",
+    help="the directory to output to, will be created if it doesn't exist, "
+    "defaults to output")
+parser.add_argument("-d", "--dont-copy", action="store_false", dest="copy",
+    default=True, help="don't copy nonhtml files to output dir"
+)
+parser.add_argument("-k", "--keep-folder", action="store_false", dest="clear",
+    default=True, help="clear the output folder if it exists, implies -d")
+
+args = parser.parse_args()
 
 
-frame = open("frame.html").read()
-header = open("header.html").read()
-bubble = open("bubble.html").read()
-rule = open("rule.css").read()
-
-if len(sys.argv) > 1:
-    source = sys.argv[1]
-if len(sys.argv) == 3:
-    dest = sys.argv[2]
+if args.clear:
+    print("clearing output folder")
+    try:
+        shutil.rmtree(args.dest)
+    except OSError:
+        print("it does not exist")
+    if args.copy:
+        print("copying nonhtml files into output folder")
+        shutil.copytree("nonhtml", args.dest)
 else:
-    dest = "output"
+    if args.copy:
+        print("not copying nonhtml files")
+    try:
+        print("making output folder:", args.dest)
+        os.mkdir(args.dest)
+    except OSError:
+        print("it already exists")
 
-try:
-    print("making folder:", dest)
-    os.mkdir(dest)
-except OSError:
-    print("it already exists")
-
-files = os.listdir(source)
-print("files source in folder:", files)
+files = os.listdir(args.source)
+print("source files in folder:", files)
 for filename in files:
-    pagename, extension = filename.split(".")
+    pagename, extension = filename.rsplit(".", 1)
+    pageinfo = None
     if extension == "json":
-        print("building", pagename)
-        pageinfo = json.load(open(source + os.sep + filename))
+        pageinfo = json.load(open(args.source + os.sep + filename))
+        print("loading", filename, "as JSON")
+    elif extension =="yaml":
+        pageinfo = yaml.load(open(args.source + os.sep + filename))
+        print("loading", filename, "as YAML")
+    else:
+        print("unknown extension", extension + ", skipping file", filename)
+    if pageinfo:
         print("opening css")
-        css = open(dest + os.sep + pagename + ".css", "w")
+        css = open(args.dest + os.sep + pagename + ".css", "w")
         css.write(pageinfo.pop("CSS") + "\n")
         htmlcontent = ""
         pagetitle = pageinfo.pop("title")
@@ -41,9 +79,11 @@ for filename in files:
                 attrs["style"] = " style=\"" + attrs["style"] + "\""
             else:
                 attrs["style"] = ""
+            if "\n" in attrs["content"]:
+                attrs["content"] = markdown.markdown(attrs["content"])
             htmlcontent += bubble.format(id=objectid + "-bubble", **attrs)
         print("writing html")
-        open(dest + os.sep + pagename + ".html", "w").write(frame.format(
+        open(args.dest + os.sep + pagename + ".html", "w").write(frame.format(
             title=pagetitle,
             stylesheet=pagename + ".css",
             header=header,
