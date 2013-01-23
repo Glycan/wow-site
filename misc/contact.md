299b3b689d998d3dc32f15b37245aa5bc67fcada
Matthew Liberman <wisepoison@gmail.com>
2013-01-23 12:04:11 -0500

    Finished major pages

---
+++ misc/contact.md
@@ -0,0 +1,21 @@
+USA
+===
+
+ - New York NY:      646-396-9366
+
+ - Boston MA:        617-201-2525 or 617-208-5820
+
+ - Albany NY:        518-795-9300
+
+ - Burlington VT:    802-489-0375
+
+
+Canada
+======
+
+ - Montreal:         514-500-6524
+
+ - Ottawa:           438-889-8883
+
+ - Quebec:           438-889-8883
+
--- templater.py
+++ templater.py
@@ -18,7 +18,7 @@ rule = open("templates/rule.css").read()
 
 header = open("other/header.html").read()
 footer = open("other/footer.html").read()
-
+form = open("other/form.html").read()
 
 parser = ArgumentParser(description="""
 Put togeather a site from source files. The contents of the nonhtml folder
@@ -31,15 +31,17 @@ file. A source file should also have a title attribute, which will be it's
 title
 """)
 parser.add_argument("source", default="source", nargs="?",
-    help="a directory with the source files in JSON or YAML, defaults to source")
+    help="a directory with the source files in JSON or YAML, or path to one "
+    "such file, defaults to source. Files *must* have extensions")
 
 parser.add_argument("dest", default="output", nargs="?",
     help="the directory to output to, will be created if it doesn't exist, "
     "defaults to output")
 
 parser.add_argument("-d", "--dont-copy", action="store_false", dest="copy",
-    default=True, help="don't copy nonhtml files to output dir"
-)
+    default=True, help="don't copy nonhtml files to output dir")
+parser.add_argument("-l", "--leave-less", action="store_false", dest="less_copy",
+    default=True, help="don't compile+copy less")
 
 parser.add_argument("-k", "--keep-folder", action="store_false", dest="clear",
     default=True, help="clear the output folder if it exists, implies -d")
@@ -52,8 +54,6 @@ parser.add_argument("--debug", action="store_true", dest="debug", default=False,
 
 parser.add_argument("--compress", action="store_true", dest="compress",
     default=False, help="compress CSS outputed by LESS")
-
-
 
 
 def lessc(source):
@@ -73,6 +73,50 @@ def lessc(source):
     #output. this cuts away everything before, well, something
     return output
 
+
+def compile(path, dest):
+    filename = path.split(os.sep)[-1]
+    pagename, extension = filename.rsplit(".", 1)
+    pageinfo = None
+    if extension == "json":
+        pageinfo = json.load(open(path))
+        print("loading", filename, "as JSON")
+    elif extension =="yaml":
+        pageinfo = yaml.load(open(path), OrderedDictYAMLLoader)
+        print("loading", filename, "as YAML")
+    else:
+        print("unknown extension", extension + ", skipping file", filename)
+        return False
+    #start actually compiling
+    print("opening css")
+    css = open(dest + os.sep + "css" + os.sep + pagename + ".css", "w")
+    open("temp.less", 'w').write(pageinfo.pop("less", "") + "\n\n")
+    css.write(lessc(pageinfo.pop("CSS", "")) + "\n")
+    htmlcontent = ""
+    pagetitle = pageinfo.pop("title")
+    for objectid, attrs in pageinfo.items():
+        print("making bubble:", objectid)
+        if "style" in attrs:
+            style = lessc(rule.format(id=objectid, style=attrs.pop("style")))
+            if args.debug:
+                print(style)
+            css.write(style)
+        if "\n" in attrs["content"]:
+            attrs["content"] = markdown.markdown(attrs["content"])
+        attrs["content"] = attrs["content"].format(form=form.replace("{name}", filename))
+        htmlcontent += bubble.format(id=objectid + "-bubble", **attrs)
+    print("writing html")
+    open(dest + os.sep + pagename + ".html", "w").write(frame.format(
+        title=pagetitle,
+        stylesheet=pagename + ".css",
+        header=header,
+        footer=footer,
+        content=htmlcontent,
+    ))
+    print("closing css")
+    css.close()
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
@@ -89,60 +133,36 @@ if __name__ == "__main__":
         if args.copy:
             print("copying nonhtml files into output folder")
             shutil.copytree("nonhtml", args.dest)
-            for fname in os.listdir("less"):
-                print("less is compiling", fname)
-                destname = args.dest + os.sep + fname.split(".")[0] + ".css"
-                call(["lessc", "--yui-compress", "less" + os.sep + fname, destname])
         else:
             print("not copying nonhtml files because -d")
+        print("making css")
+        os.mkdir(args.dest + os.sep + "css")
+    if args.less_copy:
+        for fname in os.listdir("less"):
+            print("less is compiling", fname)
+            destname = args.dest + os.sep + "css" + os.sep + fname.split(".")[0] + ".css"
+            arg = ""
+            if args.compress:
+                arg = "--compress"
+                print("compressing less")
+            call(["lessc", arg, "less" + os.sep + fname, destname])
     else:
         print("not copying nonhtml files because -k")
         try:
-            print("making output folder:", args.dest)
+            print("making output and css folder:", args.dest)
             os.mkdir(args.dest)
+            os.mkdir(args.dest + os.sep + "css")
         except OSError:
             print("it already exists")
+    if "." in args.source:
+        print("compiling one file", args.source)
+        compile(args.source, args.dest)
+    else:
+        files = os.listdir(args.source)
+        print("source files in folder:", files)
+        for filename in files:
+            print("compiling", filename)
+            compile(args.source + os.sep + filename, args.dest)
 
-    files = os.listdir(args.source)
-    print("source files in folder:", files)
-    for filename in files:
-        pagename, extension = filename.rsplit(".", 1)
-        pageinfo = None
-        if extension == "json":
-            pageinfo = json.load(open(args.source + os.sep + filename))
-            print("loading", filename, "as JSON")
-        elif extension =="yaml":
-            pageinfo = yaml.load(open(args.source + os.sep + filename), OrderedDictYAMLLoader)
-            print("loading", filename, "as YAML")
-        else:
-            print("unknown extension", extension + ", skipping file", filename)
-        if pageinfo:
-            print("opening css")
-            css = open(args.dest + os.sep + pagename + ".css", "w")
-            open("temp.less", 'w').write(pageinfo.pop("less", "") + "\n\n")
-            css.write(lessc(pageinfo.pop("CSS", "")) + "\n")
-            htmlcontent = ""
-            pagetitle = pageinfo.pop("title")
-            JS = pageinfo.pop("JS", "")
-            for objectid, attrs in pageinfo.items():
-                print("making bubble:", objectid)
-                if "style" in attrs:
-                    style = lessc(rule.format(id=objectid, style=attrs.pop("style")))
-                    if args.debug:
-                        print(style)
-                    css.write(style)
-                if "\n" in attrs["content"]:
-                    attrs["content"] = markdown.markdown(attrs["content"])
-                htmlcontent += bubble.format(id=objectid + "-bubble", **attrs)
-            print("writing html")
-            open(args.dest + os.sep + pagename + ".html", "w").write(frame.format(
-                title=pagetitle,
-                stylesheet=pagename + ".css",
-                header=header,
-                footer=footer,
-                content=htmlcontent,
-                JS=JS
-            ))
-            print("closing css")
-            css.close()
 os.remove("temp.less")
+print("DONE.\n\n\n")
